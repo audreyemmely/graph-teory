ca92999f3f9b921d77400a88ab727eaf9a40c5f5
Audrey <aerv@ic.ufal.br>
2021-09-10 19:52:26 -0300

    Create graph.c

---
+++ dijkstra/graph.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <graph.h>
+
+Graph *createGraph(int numberOfVertices, int maxDegree, int isWeighted, int isDigraph)
+{
+    Graph *gr = (Graph*)malloc(sizeof(struct graph));
+
+    if(gr != NULL)
+    {
+        int i, j;
+        gr->numberOfVertices = numberOfVertices;
+        gr->maxDegree = maxDegree;
+        gr->isWeighted = isWeighted;
+        gr->isDigraph = isDigraph;
+        gr->degree = (int*)calloc(numberOfVertices, sizeof(int));
+        gr->edges = (int**)malloc(numberOfVertices*sizeof(int*));
+
+        for(i = 0; i < numberOfVertices; i++)
+        {
+            gr->edges[i] = (int*)malloc(maxDegree*sizeof(int));
+
+            if(gr->isWeighted)
+            {
+                gr->weight = (int**)malloc(numberOfVertices*sizeof(int*));
+
+                for (j = 0; j < numberOfVertices; j++)
+                {
+                    gr->weight[j] = (int*)malloc(maxDegree*sizeof(int));
+                }
+                
+            }
+        }
+    }
+}
